76e8d56777d916cd19e9a86470d8214433ff6aa5
Ferney Medina <fermed28@gmail.com>
2019-10-04 00:35:02 +0000

    [add] inital boilerplate

---
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -0,0 +1,20 @@
+// Listen for changes on each INPUT checkbox tag
+const amenIds = {};
+
+$(function () {
+  $('.popover li input[type=checkbox]').on('change', function () {
+    if ($(this).is(':checked')) {
+      amenIds[$(this).attr('data-id')] = $(this).attr('data-name');
+    } else {
+      delete amenIds[$(this).attr('data-id')];
+    }
+    const values = Object.values(amenIds);
+    // $('.amenities h4').text(values.join());
+    if (values.length > 0) {
+      $('.amenities h4').text(values.join(', '));
+    } else {
+      $('.amenities h4').html('&nbsp;');
+    }
+    console.log(amenIds);
+  });
+});

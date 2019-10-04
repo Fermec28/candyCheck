3605268e67b0cb0f395ad076611d182e476decb9
Maria Alejandra Coy Ulloa <764@holbertonschool.com>
2019-10-04 17:16:14 +0000

    [Feature] correction of the __init__ of the models module

--- models/__init__.py
+++ models/__init__.py
@@ -1,15 +1,14 @@
 import os
 from models.base_model import BaseModel
-from models.amenity import Amenity
-from models.city import City
-from models.place import Place
-from models.review import Review
-from models.state import State
 from models.user import User
+from model.projects import Project
+from models.task import Task
+from models.corrections import Correction
+from models.check import Check
 
 """CNC - dictionary = { Class Name (string) : Class Type }"""
 
-if os.environ.get('HBNB_TYPE_STORAGE') == 'db':
+if os.environ.get('CANDY_TYPE_STORAGE') == 'db':
     from models.engine import db_storage
     CNC = db_storage.DBStorage.CNC
     storage = db_storage.DBStorage()

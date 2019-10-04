1f7b1ae6c87f30dc786ded9734c081b46ddb8761
Maria Alejandra Coy Ulloa <764@holbertonschool.com>
2019-10-04 16:42:56 +0000

    [Feature] create the model of the correction table, has the information ofeach correction

---
+++ models/corrections.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""
+State Class from Models Module
+"""
+import os
+import models
+from models.base_model import BaseModel, Base
+from sqlalchemy.orm import relationship
+from sqlalchemy import Column, Integer, String, Float
+STORAGE_TYPE = os.environ.get('CANDY_TYPE_STORAGE')
+
+
+class Correction(BaseModel, Base):
+    """Correction class handles all application correction"""
+    if STORAGE_TYPE == "db":
+        __tablename__ = 'correction'
+        status = Column(String(10), nullable=False)
+        user_id = Column(String(60), ForeignKey('user.id'), nullable=False)
+        task_id = Column(String(60), ForeignKey('task.id'), nullable=False)
+        project_id = Column(String(60), ForeignKey('project.id'), nullable=False)
+        checks = relationship('Check', backref='correction', cascade='delete')
+    else:
+        status = ''
+
+        @property
+        def checks(self):
+            """
+                getter method, returns list of Check objs from storage
+                linked to the current State
+            """
+            check_list = []
+            for check in models.storage.all("Check").values():
+                if check.project_id == self.id:
+                    check_list.append(check)
+            return check_list

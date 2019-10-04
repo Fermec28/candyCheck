eeb51971eca41b86a461ef4fe2cf5b9dd76c7f40
Maria Alejandra Coy <764@holbertonschool.com>
2019-10-04 20:42:25 +0000

    [Fix] fixing the type of the column for a Bolean type

--- models/check.py
+++ models/check.py
@@ -5,7 +5,7 @@ Check Class from Models Module
 import os
 from models.base_model import BaseModel, Base
 from sqlalchemy.orm import relationship
-from sqlalchemy import Column, Integer, String, Float, ForeignKey
+from sqlalchemy import Column, Integer, String, Float, ForeignKey, Boolean
 STORAGE_TYPE = os.environ.get('CANDY_TYPE_STORAGE')
 
 
@@ -14,7 +14,7 @@ class Check(BaseModel, Base):
     if STORAGE_TYPE == "db":
         __tablename__ = 'check'
         title = Column(String(128), nullable=False)
-        passed = Column(Integer, nullable=False)
+        passed = Column(Boolean, nullable=False)
         correction_id = Column(Integer, ForeignKey('correction.id'), nullable=False)
     else:
                 title = ''

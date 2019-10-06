fd883f85a57d96b9b29d0e2dbd56813f441a102e
David Latorre <david@latorredev.com>
2019-10-06 02:59:21 -0500

    [FEATURE] add break points for each option

--- holbi_docker_script.sh
+++ holbi_docker_script.sh
@@ -13,20 +13,25 @@ do                                                                              
 echo 'creating docker volume'
 docker volume create holbihack
 echo 'volume created'
+break
 ;;
 
 "Download container")
 echo "Downloading container"
 docker pull fermec28/flask-mysql-ready
 echo "Container downloaded"
+break
 ;;
 
 "Run container")
 sudo docker run -p 5000:5000 -p 5001:5001 -d -it -v holbihack:/root/holbihack fermec28/flask-mysql-ready
+echo "Container running"
+break
 ;;
 
 "Connect to the container")
 docker exec -it $(docker ps | tail -1 | rev | cut -d " " -f 1 | rev) /bin/bash
+break
 ;;
 
 "Exit")
